1b43bb7ba859f0cb0a3f15a901d420789b95c604
Angel <angel.torres.moreira@gmail.com>
2019-11-19 21:38:31 +0000

    modified install.sh

--- install.sh
+++ install.sh
@@ -13,11 +13,16 @@ else
     sudo /etc/init.d/docker start
 fi
 #docker compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
+if [ -x "$(command -v docker)" ]; then
+    echo "Update docker compose"
+    # command
+else
+    echo "Install docker compose"
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+fi
 #clone repo and setup images
-git clone https://github.com/angelisimo/syschallenge.git
+#git clone https://github.com/angelisimo/syschallenge.git
 
 #start compose
 docker-compose -f /pythonapp/docker-compose.yml
